58ed9e6200b66e7b708a7f3fae7e8671a6f11c02
igor <igoriokas@gmail.com>
2023-11-27 16:15:35 +0200

    osd

--- mpvnet/start.bat
+++ mpvnet/start.bat
@@ -1,2 +1,1 @@
-@REM %comspec% /k .\venv382\scripts\python .\start.py
-mpvnet --input-conf=input.conf --script=scripts/my.lua rtsp://192.168.0.101:554/ch0.liv
+mpvnet --osd-font-size=24 --no-osc --osd-duration=500 --input-conf=input.conf --script=scripts/my.lua rtsp://192.168.0.101:554/ch0.liv
